7e8f6d52e06afdb6eb0d17be1fb03c3861142a3e
PepsodenTBiawaK <adilfadhilah03@gmail.com>
2025-04-21 15:47:32 +0700

    ubah baseURL

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("/api/auth/login", {
         email,
         password,
       });
